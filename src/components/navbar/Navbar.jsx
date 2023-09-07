d9dfdba4cdf7c47db9dcca8987fa6d16fe28f43a
Viana Ferguson <vianaferguson@yahoo.com>
2023-09-06 23:01:00 -0700

    possibility css 2:14

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import './navbar.css'
 
 const Menu = () => (
   <>
-  <p><a href='#'>Home</a></p>
-  <p><a href='#'>What is GPT?</a></p>
+  <p><a href='#home'>Home</a></p>
+  <p><a href='#wgpt3'>What is GPT?</a></p>
   <p><a href='#possibility'>Open AI</a></p>
   <p><a href='#features'>Case Studies</a></p>
   <p><a href='#blog'>Library</a></p>
